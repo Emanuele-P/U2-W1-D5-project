1062d365c13f62b70193ae20009198aae2139ff2
Emanuele-P <161461547+Emanuele-P@users.noreply.github.com>
2024-03-29 18:46:44 +0100

    fixed

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const navBar = document.querySelector('header')
 
+let scrollPosition = window.scrollY || document.documentElement.scrollTop
+const changeHeight = document.querySelector('.trending-wrapper').offsetHeight
+
+if (scrollPosition > changeHeight) {
+  navBar.classList.add('scrolled')
+} else {
+  navBar.classList.remove('scrolled')
+}
+
 window.addEventListener('scroll', function () {
   let scrollPosition = window.scrollY || this.documentElement.scrollTop
 
@@ -11,6 +20,7 @@ window.addEventListener('scroll', function () {
   }
 })
 
+/*
 const assignRandom = function () {
   const elements = document.querySelectorAll('.artwork svg path')
 
@@ -18,11 +28,26 @@ const assignRandom = function () {
     const opacity = [0, 1]
 
     for (let i = 0; i < elements.length; i++) {
-      let randomIndex = Math.floor(Math.random() * opacity.length)
+      let randomIndex = opacity[Math.floor(Math.random() * opacity.length)]
       elements[i].style.opacity = opacity[randomIndex]
       elements[i].style.transitionDelay = Math.random() * 5 + 's'
     }
   }
 }
 
-setInterval(assignRandom, 2000)
+setInterval(assignRandom, 2000) */
+
+const assignRandom = function () {
+  const elements = document.querySelectorAll('.artwork svg path')
+
+  for (let i = 0; i < elements.length; i++) {
+    if (Math.random() > 0.5) {
+      elements[i].style.opacity = 1
+    } else {
+      elements[i].style.opacity = 0
+    }
+    elements[i].style.transitionDelay = Math.random() * 5 + 's'
+  }
+}
+
+setInterval(assignRandom, 1000)

de939195447a07d6afc3841614241f3b4ddcd82d
Emanuele-P <161461547+Emanuele-P@users.noreply.github.com>
2024-03-29 16:41:48 +0100

    added animation

--- index.js
+++ index.js
@@ -10,3 +10,19 @@ window.addEventListener('scroll', function () {
     navBar.classList.remove('scrolled')
   }
 })
+
+const assignRandom = function () {
+  const elements = document.querySelectorAll('.artwork svg path')
+
+  for (let i = 0; i < elements.length; i++) {
+    const opacity = [0, 1]
+
+    for (let i = 0; i < elements.length; i++) {
+      let randomIndex = Math.floor(Math.random() * opacity.length)
+      elements[i].style.opacity = opacity[randomIndex]
+      elements[i].style.transitionDelay = Math.random() * 5 + 's'
+    }
+  }
+}
+
+setInterval(assignRandom, 1000)

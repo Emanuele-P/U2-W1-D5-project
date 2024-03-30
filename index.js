a327ea2b4561ea50daf3ef60888c7991a5d0f88a
Emanuele-P <161461547+Emanuele-P@users.noreply.github.com>
2024-03-30 12:56:22 +0100

    fixed animation

--- index.js
+++ index.js
@@ -20,34 +20,17 @@ window.addEventListener('scroll', function () {
   }
 })
 
-/*
-const assignRandom = function () {
-  const elements = document.querySelectorAll('.artwork svg path')
-
-  for (let i = 0; i < elements.length; i++) {
-    const opacity = [0, 1]
-
-    for (let i = 0; i < elements.length; i++) {
-      let randomIndex = opacity[Math.floor(Math.random() * opacity.length)]
-      elements[i].style.opacity = opacity[randomIndex]
-      elements[i].style.transitionDelay = Math.random() * 5 + 's'
-    }
-  }
-}
-
-setInterval(assignRandom, 2000) */
-
 const assignRandom = function () {
   const elements = document.querySelectorAll('.artwork svg path')
 
   for (let i = 0; i < elements.length; i++) {
     if (Math.random() > 0.5) {
-      elements[i].style.opacity = 1
+      elements[i].classList.add('visible')
     } else {
-      elements[i].style.opacity = 0
+      elements[i].classList.remove('visible')
     }
     elements[i].style.transitionDelay = Math.random() * 5 + 's'
   }
 }
-
-setInterval(assignRandom, 1000)
+assignRandom()
+setInterval(assignRandom, 5000)
